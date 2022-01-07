c28c82fcc39d88d25d8921fdc9cedac6e438b523
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-01-07 20:11:11 +0800

    移除不必要的数组并在插件禁用时清除配方

--- src/main/java/me/dreamvoid/bundlerecipe/BukkitPlugin.java
+++ src/main/java/me/dreamvoid/bundlerecipe/BukkitPlugin.java
@@ -7,15 +7,18 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
-
 public class BukkitPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "bundle"), new ItemStack(Material.BUNDLE));
-        recipe.shape(Arrays.asList("SRS","R R","RRR").toArray(new String[0]));
-        recipe.setIngredient('S',Material.STRING);
-        recipe.setIngredient('R',Material.RABBIT_HIDE);
-        Bukkit.addRecipe(recipe);
+        Bukkit.addRecipe(new ShapedRecipe(new NamespacedKey(this, "bundle"), new ItemStack(Material.BUNDLE))
+                .shape("SRS", "R R", "RRR")
+                .setIngredient('S', Material.STRING)
+                .setIngredient('R', Material.RABBIT_HIDE)
+        );
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.clearRecipes();
     }
 }
